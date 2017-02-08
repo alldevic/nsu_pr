31875bf8964e2c6d7c275b0759c88127db716579
Unknown <belyynikolay@gmail.com>
2017-02-09 03:17:18 +0700

    add read from file

--- lab1/solution.c
+++ lab1/solution.c
@@ -2,9 +2,94 @@
  * Laboratory work #1
  * Dijkstra algorithm
  */
+
+#include <stdio.h>
+#include <malloc.h>
 #include "solution.h"
 
-int main(void)
-{
-    return 0;
+int main(void) {
+    Graph g = malloc(sizeof(Graph));
+    int s = 0, f = 0;
+    char *error;
+    int err = getGraphFromFile(g, &s, &f);
+    if (!(err % ERR_ARG)) {
+        error = getBadArgAnswer((ArgError) (err / ERR_ARG));
+    }
+
+    return err;
+}
+
+/**
+ * @function Get data from <b>INPUT</b>
+ * @param g - information about count of vertexes and all about edges
+ * @param s  begin vertex
+ * @param f - destination vertex
+ * @return error code
+ */
+int getGraphFromFile(Graph g, int *s, int *f) {
+    int i = 0, j = 0;
+    int a = 0, b = 0, c = 0;
+    FILE *file = fopen(INPUT, "r");
+    ERR(file != NULL, ERR_OPEN);
+    /*Read 1s line*/
+    ERR(fscanf(file, "%d", &(g->n)) == 1, ERR_SCAN);
+    ERR((g->n >= 0) && (g->n <= MAX_VERTEX), BAD_NV * ERR_ARG);
+
+    /*Read 2nd line*/
+    ERR(fscanf(file, "%d %d", s, f) == 2, ERR_SCAN);
+    ERR((*s > 0) && (*s <= g->n), BAD_V * ERR_ARG);
+    ERR((*f > 0) && (*f <= g->n), BAD_V * ERR_ARG);
+
+    /*Read 3rd line*/
+    ERR(fscanf(file, "%d", &(g->m)) == 1, ERR_SCAN);
+    ERR((g->m >= 0) && (g->m <= (g->n * (g->n + 1) / 2)), BAD_NE * ERR_ARG);
+
+    /*Allocation memory for edges array*/
+    g->edges = (int **) malloc(g->n * sizeof(int *));
+    ERR(g->edges != NULL, ERR_ALLOC);
+
+    for (i = 0; i < g->n; i++) {
+        g->edges[i] = (int *) malloc(g->n * sizeof(int));
+        ERR(g->edges[i] != NULL, ERR_ALLOC);
+        for (j = 0; j < g->n; j++) {
+            g->edges[i][j] = INT_MAX;
+        }
+    }
+
+    /*Read edges data*/
+    i = 0;
+    while (!feof(file)) {
+        ERR(fscanf(file, "%d %d %d", &a, &b, &c) == 3, ERR_SCAN);
+        ERR((a > 0) && (a <= g->n), BAD_V * ERR_ARG);
+        ERR((b > 0) && (b <= g->n), BAD_V * ERR_ARG);
+        ERR((c >= 0) && (b <= INT_MAX), BAD_LEN * ERR_ARG);
+        g->edges[a - 1][b - 1] = c;
+        i++;
+    }
+    ERR(i == g->m, BAD_NL * ERR_ARG);
+
+    ERR(fclose(file) == EOF, ERR_CLOSE);
+    return ERR_NO;
+}
+
+/**
+ * @function Matching <b>ARG_ERRORS</b> value with string interpretation
+ * @param err - ARG_ERROR code
+ * @return string interpretation
+ */
+char *getBadArgAnswer(enum ARG_ERRORS err) {
+    switch (err) {
+
+        case BAD_NV:
+            return BAD_NV_ANS;
+        case BAD_V:
+            return BAD_V_ANS;
+        case BAD_NE:
+            return BAD_NE_ANS;
+        case BAD_LEN:
+            return BAD_LEN_ANS;
+        case BAD_NL:
+            return BAD_NL_ANS;
+    }
+    return NULL;
 }
--- lab1/solution.h
+++ lab1/solution.h
@@ -14,8 +14,18 @@
 #define OUTPUT "out.txt"
 
 /**
+ * @def Max number of vertexes in graph
+ */
+#define MAX_VERTEX 5000
+
+/**
+ * @def Length of edge between unreachable vertexes
+ */
+#define INT_MAX 2147483647
+
+/**
  * @enum List of errors:<ul>
- * <li>NO_ERR - successful
+ * <li>ERR_NO - successful
  * <li>ERR_OPEN - problem with <b>INPUT</b>
  * <li>ERR_CREATE - problem with <b>OUTPUT</b>
  * <li>ERR_SCAN - can't read from <b>INPUT</b>
@@ -23,15 +33,64 @@
  * <li>ERR_CLOSE - can't correct close file
  * <li>ERR_ALLOC - can't allocation memory
  * <li>ERR_NULL - nullable argument
+ * <li>ERR_ARG - bad argument
  */
-enum ERRORS {
-    NO_ERR, ERR_OPEN, ERR_CREATE, ERR_SCAN,
-    ERR_PRINT, ERR_CLOSE, ERR_ALLOC, ERR_NULL
-};
+typedef enum ERRORS {
+    ERR_NO, ERR_OPEN, ERR_CREATE, ERR_SCAN,
+    ERR_PRINT, ERR_CLOSE, ERR_ALLOC, ERR_NULL,
+    ERR_ARG
+} Error;
+
+typedef enum ARG_ERRORS {
+    BAD_NV = 1, BAD_V, BAD_NE, BAD_LEN, BAD_NL
+} ArgError;
 
 /**
- * @def Macros for trapping errors from <bb>ERRORS</b> enum
+ * @def String interpretation of <b>BAD_NV</b>
+ * in <b>ARG_ERRORS</b> enum
  */
-#define ERR(x,y) if (!x) return y;
+#define BAD_NV_ANS "bad number of vertices"
+
+/**
+ * @def String interpretation of <b>BAD_V</b>
+ * in <b>ARG_ERRORS</b> enum
+ */
+#define BAD_V_ANS "bad vertex"
+
+/**
+ * @def String interpretation of <b>BAD_NE</b>
+ * in <b>ARG_ERRORS</b> enum
+ */
+#define BAD_NE_ANS "bad number of edges"
+
+/**
+ * @def String interpretation of <b>BAD_LEN</b>
+ * in <b>ARG_ERRORS</b> enum
+ */
+#define BAD_LEN_ANS "bad length"
+
+/**
+ * @def String interpretation of <b>BAD_NL</b>
+ * in <b>ARG_ERRORS</b> enum
+ */
+#define BAD_NL_ANS "bad number of lines"
+
+/**
+ * @def Macros for trapping errors from <b>ERRORS</b> enum
+ */
+#define ERR(x, y) if (!(x)) return y;
+
+/**
+ * @typedef Structure for storage information about graph:
+ */
+typedef struct graph {
+    int n; /**Number of vertices*/
+    int m; /**Number of edges*/
+    int **edges; /**Matrix of values*/
+} *Graph;
+
+int getGraphFromFile(Graph g, int *s, int *f);
+
+char *getBadArgAnswer(ArgError err);
 
 #endif /*LAB1_SOLUTION_H*/

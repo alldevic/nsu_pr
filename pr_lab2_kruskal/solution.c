238bcdd92ee5ff1a5d419b6e3313345c882fefa4
Unknown <belyynikolay@gmail.com>
2017-02-26 04:24:43 +0700

    added realisation, not working

--- pr_lab2_kruskal/solution.c
+++ pr_lab2_kruskal/solution.c
@@ -8,19 +8,81 @@
 #define ARG_ERR(statement, code) if (statement) {return code;}
 
 int main() {
-    Graph gr = malloc(sizeof(Graph));
+    Graph *gr = malloc(sizeof(Graph));
     ERR(gr == NULL);
-    int er = readData(gr);
+    int er = readData(gr), i = 0;
+    char tmp[10] = "";
     ERR(er > 0);
     /*ARG_ERR(er < 0, printAnswer(getStrArgErr((ArgError)er), 1));*/
 
     if (er < 0)
         return printAnswer(getStrArgErr((ArgError) er), 1);
 
+    kruskal(gr);
+    printAnswer("", 1);
+    for (i = 0; i < gr->n - 1; i++) {
+        memset(tmp, 0, strlen(tmp));
+        sprintf(tmp, "%d %d\n", gr->minTree[i].src, gr->minTree[i].dest);
+        printAnswer(tmp, 0);
+    }
     return 0;
 }
 
-int readData(Graph gr) {
+int kruskal(Graph *gr) {
+    int j = 0, i = 0, v = 0;
+    qsort(gr->edges, (size_t) gr->m, sizeof(gr->edges[0]), myComp);
+    Subset *subsets = (Subset *) malloc(gr->m * sizeof(Subset));
+
+    for (v = 0; v < gr->n; ++v) {
+        subsets[v].parent = v;
+        subsets[v].rank = 0;
+    }
+
+    while (j < gr->n - 1) {
+        Edge next_edge = gr->edges[i++];
+
+        int x = find(subsets, next_edge.src);
+        int y = find(subsets, next_edge.dest);
+
+        if (x != y) {
+            gr->minTree[j++] = next_edge;
+            Union(subsets, x, y);
+        }
+    }
+
+    return 0;
+}
+
+void Union(Subset *subsets, int x, int y) {
+    int xroot = find(subsets, x);
+    int yroot = find(subsets, y);
+
+    if (subsets[xroot].rank < subsets[yroot].rank)
+        subsets[xroot].parent = yroot;
+    else if (subsets[xroot].rank > subsets[yroot].rank)
+        subsets[yroot].parent = xroot;
+    else {
+        subsets[yroot].parent = xroot;
+        subsets[xroot].rank++;
+    }
+}
+
+int find(Subset *subsets, int x) {
+
+
+    if (subsets[x].parent != x)
+        subsets[x].parent = find(subsets, subsets[x].parent);
+
+    return subsets[x].parent;
+}
+
+int myComp(const void *a, const void *b) {
+    Edge *a1 = (Edge *) a;
+    Edge *b1 = (Edge *) b;
+    return a1->weight > b1->weight;
+}
+
+int readData(Graph *gr) {
     int i = 0, a = 0, b = 0, c = 0;
 
     FILE *file = fopen(INPUT, "r");
@@ -35,7 +97,8 @@ int readData(Graph gr) {
     ARG_ERR((gr->m < 0) || (gr->m > (gr->n * (gr->n + 1) / 2)), BAD_NE);
 
     /*Read edges data*/
-    ERR(initArrays(gr) != 0);
+    ERR((gr->edges = (Edge *) malloc(gr->n * sizeof(Edge))) == NULL);
+    ERR((gr->minTree = (Edge *) malloc(gr->n * sizeof(Edge))) == NULL);
     ARG_ERR(gr->m == 0, 0);
     i = 0;
     while (!feof(file)) {
@@ -44,9 +107,9 @@ int readData(Graph gr) {
         ARG_ERR(((b < 1) || (b > gr->n)), BAD_V);
         ARG_ERR((c < 0) || (c > MAX_INT), BAD_LEN);
         if (a != b) {
-            gr->edges[a - 1][b - 1] = (unsigned int) c;
-            gr->edges[b - 1][a - 1] = (unsigned int) c;
-
+            gr->edges[i].src = a;
+            gr->edges[i].dest = b;
+            gr->edges[i].weight = (unsigned int) c;
         }
 
         i++;
@@ -54,20 +117,6 @@ int readData(Graph gr) {
     ARG_ERR(i != (gr->m), BAD_NL);
 
     ERR(fclose(file) != 0);
-    return 0;
-}
-
-int initArrays(Graph gr) {
-    int i = 0, j = 0;
-    ERR((gr->edges = (unsigned int **) malloc(gr->n * sizeof(int *))) == NULL);
-
-    for (i = 0; i < gr->n; i++) {
-        ERR((gr->edges[i] = (unsigned int *) malloc(gr->n * sizeof(int))) == NULL);
-
-        for (j = 0; j < gr->n; j++)
-            gr->edges[i][j] = INFTY;
-    }
-
     return 0;
 }
 
--- pr_lab2_kruskal/solution.h
+++ pr_lab2_kruskal/solution.h
@@ -10,20 +10,38 @@
 #define INFTY (2U*MAX_INT)+1
 #define NO_PATH -1
 
-typedef enum
-{
+typedef enum {
     BAD_NV = -10, BAD_V, BAD_NE, BAD_LEN, BAD_NL
 } ArgError;
 
 typedef struct {
-    int n,m;
-    unsigned int **edges;
-} *Graph;
+    int src, dest;
+    unsigned int weight;
+} Edge;
+
+typedef struct {
+    int n, m;
+    Edge *edges, *minTree;
+} Graph;
+
+typedef struct {
+    int parent;
+    int rank;
+} Subset;
 
 
-int readData(Graph gr);
-int initArrays(Graph gr);
-char* getStrArgErr(ArgError arg);
+int readData(Graph *gr);
+
+int kruskal(Graph *gr);
+
+int myComp(const void* a, const void* b);
+
+int find(Subset *subsets, int i);
+
+void Union(Subset *subsets, int x, int y);
+
+char *getStrArgErr(ArgError arg);
+
 int printAnswer(char *str, int fl);
 
 #endif /*LAB2_SOLUTION_H*/

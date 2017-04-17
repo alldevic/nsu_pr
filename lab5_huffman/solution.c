34715be7fb62ab313d6f719ba3c99bd9834a0cb5
Nikolay Bely <belyynikolay@gmail.com>
2017-04-17 12:18:49 +0700

    lab5: add base algorithm

--- lab5_huffman/solution.c
+++ lab5_huffman/solution.c
@@ -1,7 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "solution.h"
 
-int main(void)
-{
-	printf("Hello world!\n");
-	return 0;
+int main(void) {
+    FILE *file = fopen(INPUT, "r");
+    int *freq = calloc(ALPH_SIZE, sizeof(int));
+    char type;
+    fscanf(file, "%c", &type);
+
+    if (type == CODE) {
+        init_freq(file, freq);
+        Tree tree = NULL;
+        huffman_tree_init(tree, freq);
+        FILE *out = fopen(OUTPUT, "w");
+        fprint_tree(out, tree);
+        fcode(file, tree, out);
+    }
+
+    if (type == DECODE) {
+        Tree tree = NULL;
+        fscan_tree(file, tree);
+        FILE *out = fopen(OUTPUT, "w");
+        fdecode(file, tree, out);
+    }
+    
+    return 0;
 }
--- lab5_huffman/solution.h
+++ lab5_huffman/solution.h
@@ -3,7 +3,24 @@
 
 #define INPUT "in.txt"
 #define OUTPUT "out.txt"
+#define ALPH_SIZE 256
 #define CODE 'c'
 #define DECODE 'd'
+
+typedef struct tree {
+
+} *Tree;
+
+int init_freq(FILE *file, int *freq);
+
+int huffman_tree_init(Tree tree, int *freq);
+
+int fprint_tree(FILE *file, Tree tree);
+
+int fcode(FILE *in, Tree tree, FILE *out);
+
+int fscan_tree(FILE *file, Tree tree);
+
+int fdecode(FILE *in, Tree tree, FILE* out);
 
 #endif //LAB5_SOLUTION_H
